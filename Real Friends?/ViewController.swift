239d873327ab57a8a2a0defe84951050fbeed10d
Smileydude <idsmileydudeid@gmail.com>
2015-09-15 15:26:13 -0400

    Initial Commit

---
+++ Real Friends?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Real Friends?
+//
+//  Created by Tom Woudenberg on 2015-09-15.
+//  Copyright © 2015 Smileydude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Real Friends?Tests/Real_Friends_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Real_Friends_Tests.swift
+//  Real Friends?Tests
+//
+//  Created by Tom Woudenberg on 2015-09-15.
+//  Copyright © 2015 Smileydude. All rights reserved.
+//
+
+import XCTest
+@testable import Real_Friends_
+
+class Real_Friends_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
